376a88f1bcd51c6676915914c83df303adc23d01
kundan <kundan@hivanya.com>
2024-07-11 19:09:34 -0300

    HVN-24

--- clients/slack/src/slack_process_lambda.py
+++ clients/slack/src/slack_process_lambda.py
@@ -1,50 +1,43 @@
 import os
 import json
 import boto3
-import requests
+import logging
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
 from query_engine import core  as qe_core
 
-# Initialize AWS clients
-secrets_client = boto3.client('secretsmanager')
-
-# Get Slack secrets from AWS Secrets Manager
-def get_slack_secrets(secret_name):
-    response = secrets_client.get_secret_value(SecretId=secret_name)
-    return json.loads(response['SecretString'])
-
-# Lambda handler
-def send_slack_message(channel_id, text):
-    slack_secrets = get_slack_secrets(os.environ['SLACK_SECRETS_NAME'])
-    slack_token = slack_secrets['botToken']
-
-    try:
-        # response = requests.post(
-        #     "https://slack.com/api/chat.postMessage",
-        #     json={'channel': channel_id, 'text': text},
-        #     headers={'Authorization': f'Bearer {slack_token}', 'Content-Type': 'application/json'}
-        # )
-        # response.raise_for_status()
-        # return {'statusCode': response.status_code, 'body': response.json()}
-        return {'statusCode': 200, 'body': "Uncomment posting to slack"}
-    except requests.exceptions.HTTPError as err:
-        #return {'statusCode': response.status_code, 'body': {'error': str(err)}}
-        return {'statusCode': 200, 'body': {'error': str(err)}}
-    except Exception as e:
-        return {'statusCode': 500, 'body': {'error': str(e)}}
+SlackRequestHandler.clear_all_log_handlers()
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
 
 
-def handler(event, context):
-    print(f"Event: {event}")
-    channel_id = event["request"]["channelId"]
-    text = event['request']['text']
-    is_bot = event['request']['is_bot']
+IGNORED_MESSAGE_EVENTS = ("bot_message", "message_deleted")
+SLACK_BOT_SECRET = json.loads(
+    boto3.client('secretsmanager').get_secret_value(
+        SecretId=os.environ['SLACK_SECRETS_NAME']
+    )['SecretString']
+)
+os.environ["SLACK_SIGNING_SECRET"] = SLACK_BOT_SECRET['signingSecret']
+os.environ["SLACK_BOT_TOKEN"] = SLACK_BOT_SECRET['botToken']
 
-    if not is_bot:
-        processed_text = qe_core.generate_reponse(text)
-        return send_slack_message(channel_id, processed_text)
+app = App(
+    process_before_response=True,
+    token=SLACK_BOT_SECRET['botToken'],
+    signing_secret=SLACK_BOT_SECRET['signingSecret'],
+)
 
-    return {
-        'statusCode': 200,
-        'body': {'message': "Slack-bot message event"}
-    }
+@app.event(
+        "message",
+        matchers=[lambda message: message.get("subtype") not in IGNORED_MESSAGE_EVENTS]
+)
+def handle_message(body, say, logger):
+    logger.info(body)
+    reply = qe_core.generate_reponse(body)
+    say(f"{reply}")
+
+def lambda_handler(event, context):
+    slack_handler = SlackRequestHandler(app=app)
+    slack_response = slack_handler.handle(event, context)
+    print(f"slack_response: {slack_response}")
+    return slack_response
+
